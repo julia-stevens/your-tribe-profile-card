5afaea557f40e71b49ac637765003f3d4cd38442
julia-stevens <juliastevens02@gmail.com>
2024-09-02 12:38:00 +0200

    Aanpassing originele visitekaartje & Start opbouw persoonlijk visitekaartje
    HTML en CSS aangepast & start eigen code

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') // nieuwe variabele 'expandButton' zoek naar 'header button'
+expandButton.addEventListener('click', expand) // bij een klik (van de gebruiker) roep functie expand
 
-function expand () {
-  document.body.classList.toggle('expand')
+function expand() {
+  document.body.classList.toggle('expand') // wanneer functie geroepen wordt, zet 'expand' aan/uit > dit verwijst naar de CSS code en vergroot het visitiekaartje
 }
