4fab8105a075ba0ff37776971b53d154a0f12592
julia-stevens <juliastevens02@gmail.com>
2024-09-03 21:15:04 +0200

    Nieuwe update visite kaartje

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,47 @@
-const expandButton = document.querySelector('header button') // nieuwe variabele 'expandButton' zoek naar 'header button'
-expandButton.addEventListener('click', expand) // bij een klik (van de gebruiker) roep functie expand
+// FIGURE 1
+var figure1 = document.querySelector("#figure1");
+var desktopItems = document.querySelector("#item1");
 
-function expand() {
-  document.body.classList.toggle('expand') // wanneer functie geroepen wordt, zet 'expand' aan/uit > dit verwijst naar de CSS code en vergroot het visitiekaartje
-}
+figure1.addEventListener("click", function () {
+  desktopItems.classList.toggle("active")
+});
+
+// FIGURE 2
+var figure2 = document.querySelector("#figure2");
+var desktopItems2 = document.querySelector("#item2");
+
+figure2.addEventListener("click", function () {
+  desktopItems2.classList.toggle("active")
+});
+
+// FIGURE 3
+var figure3 = document.querySelector("#figure3");
+var desktopItems3 = document.querySelector("#item3");
+
+figure3.addEventListener("click", function () {
+  desktopItems3.classList.toggle("active")
+});
+
+// FIGURE 4
+var figure4 = document.querySelector("#figure4");
+var desktopItems4 = document.querySelector("#item4");
+
+figure4.addEventListener("click", function () {
+  desktopItems4.classList.toggle("active")
+});
+
+// FIGURE 5
+var figure5 = document.querySelector("#figure5");
+var desktopItems5 = document.querySelector("#item5");
+
+figure5.addEventListener("click", function () {
+  desktopItems5.classList.toggle("active")
+});
+
+// FIGURE 6
+var figure6 = document.querySelector("#figure6");
+var desktopItems6 = document.querySelector("#item6");
+
+figure6.addEventListener("click", function () {
+  desktopItems6.classList.toggle("active")
+});
