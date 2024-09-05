24675acc4a1c0819cc9f4d4e6a724e6e5c8885d5
julia-stevens <juliastevens02@gmail.com>
2024-09-05 19:32:12 +0200

    comments added and code sorted

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-
-
+// DESKTOP ITEMS CONTENT WINDOWS appear when DESKTOP ITEM CLICKED
 var figure1 = document.querySelector("#figure1");
 var desktopItems = document.querySelector("#item1");
 
@@ -42,9 +41,10 @@ figure6.addEventListener("click", function () {
   desktopItems6.classList.toggle("active")
 });
 
+// DESKTOP ITEMS CONTENT ANIMATION TRIGGER when BUTTON MAIN WINDOW CLICKED
 var buttonMainWindow = document.querySelector("#window-main-button");
 
-buttonMainWindow.addEventListener("click", function() {
+buttonMainWindow.addEventListener("click", function () {
   figure1.classList.toggle("clicked")
   figure2.classList.toggle("clicked")
   figure3.classList.toggle("clicked")
@@ -54,70 +54,71 @@ buttonMainWindow.addEventListener("click", function() {
 });
 
 // ANIMATION TRIGGER HOVER DESKTOP ICONS
-figure1.addEventListener("click", function() {
+figure1.addEventListener("click", function () {
   figure1.classList.toggle("active")
 });
 
-figure2.addEventListener("click", function() {
+figure2.addEventListener("click", function () {
   figure2.classList.toggle("active")
 });
 
-figure3.addEventListener("click", function() {
+figure3.addEventListener("click", function () {
   figure3.classList.toggle("active")
 });
 
-figure4.addEventListener("click", function() {
+figure4.addEventListener("click", function () {
   figure4.classList.toggle("active")
 });
 
-figure5.addEventListener("click", function() {
+figure5.addEventListener("click", function () {
   figure5.classList.toggle("active")
 });
 
-figure6.addEventListener("click", function() {
+figure6.addEventListener("click", function () {
   figure6.classList.toggle("active")
 });
 
 // ANIMATION TRIGGER DESKTOP ITEMS
-item1.addEventListener("click", function() {
+item1.addEventListener("click", function () {
   item1.classList.toggle("clicked")
 });
 
-item2.addEventListener("click", function() {
+item2.addEventListener("click", function () {
   item2.classList.toggle("clicked")
 });
 
-item3.addEventListener("click", function() {
+item3.addEventListener("click", function () {
   item3.classList.toggle("clicked")
 });
 
-item4.addEventListener("click", function() {
+item4.addEventListener("click", function () {
   item4.classList.toggle("clicked")
 });
 
-item5.addEventListener("click", function() {
+item5.addEventListener("click", function () {
   item5.classList.toggle("clicked")
 });
 
-item6.addEventListener("click", function() {
+item6.addEventListener("click", function () {
   item6.classList.toggle("clicked")
 });
 
+// WINDOW CHANGES COLOR WHEN red, orange or green BUTTON CLICKED
 var buttonRed = document.querySelector("#buttonRed");
 var windowBackground = document.querySelector("#opened-window")
 
-buttonRed.addEventListener("click", function() {
-  windowBackground.classList.toggle("activeRed")  
+buttonRed.addEventListener("click", function () {
+  windowBackground.classList.toggle("activeRed")
 });
 
 var buttonOrange = document.querySelector("#buttonOrange");
 
-buttonOrange.addEventListener("click", function() {
-  windowBackground.classList.toggle("activeOrange")  
+buttonOrange.addEventListener("click", function () {
+  windowBackground.classList.toggle("activeOrange")
 });
 
 var buttonGreen = document.querySelector("#buttonGreen");
 
-buttonGreen.addEventListener("click", function() {
-  windowBackground.classList.toggle("activeGreen")  
+buttonGreen.addEventListener("click", function () {
+  windowBackground.classList.toggle("activeGreen")
 });
