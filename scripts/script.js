9c3bd830fb943ac5acc546877c5fed1a34730395
julia-stevens <juliastevens02@gmail.com>
2024-09-08 15:03:19 +0200

    HTML validator class error fix & fix JS var to const

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,49 @@
 // DESKTOP ITEMS CONTENT WINDOWS appear when DESKTOP ITEM CLICKED
-var figure1 = document.querySelector("#figure1");
-var desktopItems = document.querySelector("#item1");
+const figure1 = document.querySelector("#figure1");
+const desktopItems = document.querySelector("#item1");
+
 
 figure1.addEventListener("click", function () {
   desktopItems.classList.toggle("active")
 });
 
-var figure2 = document.querySelector("#figure2");
-var desktopItems2 = document.querySelector("#item2");
+const figure2 = document.querySelector("#figure2");
+const desktopItems2 = document.querySelector("#item2");
 
 figure2.addEventListener("click", function () {
   desktopItems2.classList.toggle("active")
 });
 
-var figure3 = document.querySelector("#figure3");
-var desktopItems3 = document.querySelector("#item3");
+const figure3 = document.querySelector("#figure3");
+const desktopItems3 = document.querySelector("#item3");
 
 figure3.addEventListener("click", function () {
   desktopItems3.classList.toggle("active")
 });
 
-var figure4 = document.querySelector("#figure4");
-var desktopItems4 = document.querySelector("#item4");
+const figure4 = document.querySelector("#figure4");
+const desktopItems4 = document.querySelector("#item4");
 
 figure4.addEventListener("click", function () {
   desktopItems4.classList.toggle("active")
 });
 
-var figure5 = document.querySelector("#figure5");
-var desktopItems5 = document.querySelector("#item5");
+const figure5 = document.querySelector("#figure5");
+const desktopItems5 = document.querySelector("#item5");
 
 figure5.addEventListener("click", function () {
   desktopItems5.classList.toggle("active")
 });
 
-var figure6 = document.querySelector("#figure6");
-var desktopItems6 = document.querySelector("#item6");
+const figure6 = document.querySelector("#figure6");
+const desktopItems6 = document.querySelector("#item6");
 
 figure6.addEventListener("click", function () {
   desktopItems6.classList.toggle("active")
 });
 
 // DESKTOP ITEMS CONTENT ANIMATION TRIGGER when BUTTON MAIN WINDOW CLICKED
-var buttonMainWindow = document.querySelector("#window-main-button");
+const buttonMainWindow = document.querySelector("#window-main-button");
 
 buttonMainWindow.addEventListener("click", function () {
   figure1.classList.toggle("clicked")
@@ -104,20 +105,20 @@ item6.addEventListener("click", function () {
 });
 
 // WINDOW CHANGES COLOR WHEN red, orange or green BUTTON CLICKED
-var buttonRed = document.querySelector("#buttonRed");
-var windowBackground = document.querySelector("#opened-window")
+const buttonRed = document.querySelector("#buttonRed");
+const windowBackground = document.querySelector("#opened-window")
 
 buttonRed.addEventListener("click", function () {
   windowBackground.classList.toggle("activeRed")
 });
 
-var buttonOrange = document.querySelector("#buttonOrange");
+const buttonOrange = document.querySelector("#buttonOrange");
 
 buttonOrange.addEventListener("click", function () {
   windowBackground.classList.toggle("activeOrange")
 });
 
-var buttonGreen = document.querySelector("#buttonGreen");
+const buttonGreen = document.querySelector("#buttonGreen");
 
 buttonGreen.addEventListener("click", function () {
   windowBackground.classList.toggle("activeGreen")
